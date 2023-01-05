4e4f4a3166d8d2a27742faad955a829a7e68be0d
Sri Annisa Falerina H <74857839+annisafalerina17@users.noreply.github.com>
2023-01-05 11:30:52 +0700

    sprint 1

---
+++ app/src/main/java/com/opendesa/demodk/ui/desa/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.opendesa.demodk.ui.desa
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.opendesa.demodk.Helper
+import com.opendesa.demodk.R
+
+
+class HomeFragment : Fragment(R.layout.fragment_desa_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.title = "Sejarah"
+        val bundle = this.arguments
+        view.findViewById<TextView>(R.id.welcome).text = bundle?.getString("sambutan_camat")
+            ?.let { Helper.fromHTML(it) }
+        val image = bundle!!.getString("logo")?.let { Helper.getImageFromURL(it) }
+        if (image != null)
+            view.findViewById<ImageView>(R.id.logoHomeView).setImageDrawable(image)
+    }
+}
