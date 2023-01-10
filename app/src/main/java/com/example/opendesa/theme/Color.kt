5300be8d0387d672f815dc267f0ca37c4f9da494
nindywahyu <nindywah@gmail.com>
2023-01-10 16:28:26 +0700

    create compose component

---
+++ app/src/main/java/com/example/opendesa/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.opendesa.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue = Color(0xFF0073b7)
+val Header = Color(0xFF2D2D2D)
+val DisableBackgroundField = Color(0xFFF5F6F8)
+val Border = Color(0xFFE5E5E5)
+val Orange = Color(0xFFf39c12)
